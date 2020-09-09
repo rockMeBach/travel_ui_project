fd019d3a79245317d6b9b2c049b272500fec19a6
Satyam Vanshal <satyamvanshal@gmail.com>
2020-09-09 17:45:26 +0530

    Setup the theme, assets and the dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
 
-void main(){
+void main() {
   runApp(TravelUI());
 }
 
-class TravelUI extends StatelessWidget{
+class TravelUI extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen()
+        title: "Flutter Travel UI",
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: Color(0xFF3EBACE),
+          accentColor: Color(0xFFD8ECF1),
+          scaffoldBackgroundColor: Color(0xFFF3F5F7),
+        ),
+        home: HomeScreen(),
     );
   }
 }
